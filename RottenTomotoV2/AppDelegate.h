f1801dd3c42bc66072c533ff53cabfca5327d417
Vijayjain <jvijay@yahoo-inc.com>
2014-09-17 23:30:11 -0700

    Initial Commit

---
+++ RottenTomotoV2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RottenTomotoV2
+//
+//  Created by Vijay Jain on 9/17/14.
+//  Copyright (c) 2014 Vijay Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
